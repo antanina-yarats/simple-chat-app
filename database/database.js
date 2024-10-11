97b993d11211754d8d48a9a0d9f8d8b2b4a58c1d
antanina-yarats <antanina.yarats@gmail.com>
2024-10-11 20:28:57 +0300

    Enable SSL for PostgreSQL connection

--- database/database.js
+++ database/database.js
@@ -2,7 +2,11 @@ import postgres from "https://deno.land/x/postgresjs@v3.4.4/mod.js";
 
 let sql;
 if (Deno.env.get("DATABASE_URL")) {
-  sql = postgres(Deno.env.get("DATABASE_URL"));
+  sql = postgres(Deno.env.get("DATABASE_URL"), {
+    ssl: {
+      rejectUnauthorized: false 
+    }
+  });
 } else {
   sql = postgres({});
 }
